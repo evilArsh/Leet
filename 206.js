2c04396367e84ef35df7087ac9cc08c9650694c3
evilArsh <1183520543@qq.com>
2020-09-20 22:08:36 +0800

    反转链表

---
+++ 206.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(head===null||head.next===null){
+        return head
+    }
+    const res=reverseList(head.next)
+    head.next.next=head
+    head.next=null
+    // let res=reverseList(head.next)
+    // let tmp=res
+    // while(tmp){
+    //     if(!tmp.next){
+    //         tmp.next=head
+    //         tmp.next.next=null
+    //         break
+    //     }
+    //     tmp=tmp.next
+    // }
+    return res
+};
